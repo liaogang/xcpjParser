2c7f6648b8c38cad501e8941244488c078837d59
liaogang <ynwlgh@hotmail.com>
2014-09-02 13:46:41 +0800

    Initial Commit

---
+++ toolxcpj/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  toolxcpj
+//
+//  Created by liaogang on 9/2/14.
+//  Copyright (c) 2014 gang.liao. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
